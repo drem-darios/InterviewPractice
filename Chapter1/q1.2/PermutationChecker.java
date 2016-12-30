a7e1d0f91db74e83210e1bb8d9a95c18c3ae08cb
Drēm Darios <vidioholic@aol.com>
2016-12-30 09:58:22 -0800

    Added comments

--- Chapter1/q1.2/PermutationChecker.java
+++ Chapter1/q1.2/PermutationChecker.java
@@ -8,29 +8,23 @@ public class PermutationChecker {
 
     public static boolean isPermutation(String firstInput, String secondInput) {
         if (firstInput.length() != secondInput.length()) {
-            return false;
+            return false; // if lengths dont match they cant be permutations
         }
 
         int[] chars = new int[256];
         for (int i = 0; i < firstInput.length(); i++) {
             int index = (int)firstInput.charAt(i);
-            chars[index]++;
+            chars[index]++;  // Count number of characters seen in first input
         }
 
         for (int i = 0; i < secondInput.length(); i++) {
             int index = (int)secondInput.charAt(i);
-            chars[index]--;
+            chars[index]--; // Subtract when matching character is found
             if (chars[index] < 0) {
-                return false;
+                return false; // odd number of characters will result in -1
             }
         }
 
         return true;
-    }
-
-    private static String sortChars(String input) {
-        char[] inputChars = input.toCharArray();
-        Arrays.sort(inputChars);
-        return new String(inputChars);
     }
 }

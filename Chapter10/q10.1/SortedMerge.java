1b50f0ed5f26029d59f495853809fcd02792069f
Drem Darios <vidioholic@aol.com>
2017-01-05 09:48:11 -0800

    Updated to more optimal solution

--- Chapter10/q10.1/SortedMerge.java
+++ Chapter10/q10.1/SortedMerge.java
@@ -10,45 +10,18 @@ public class SortedMerge {
     }
 
     public static void sortedMerge(int[] inputA, int[] inputB) {
-        int mid = inputA.length - 1;
-        for (int i = inputB.length - 1; i >= 0; i--) {
-            inputA[mid] = inputB[i]; // Fill inputA with inputB
-            mid--; // Move towards the midpoint
-        }
-        sort(inputA, 0, mid+1, inputA.length - 1);
-    }
+        int mid = inputA.length - inputB.length;
+        int currentB = inputB.length - 1;
+        int currentA = mid - 1;
 
-    private static void sort(int[] input, int low, int mid, int high) {
-        int current = 0;
-        int midIndex = mid;
-        int[] result = new int[input.length]; // O(n) memory
-
-        while (low <= midIndex && mid <= high) {
-            if (input[low] > input[mid]) {
-                result[current] = input[mid];
-                mid++;
+        for (int i = inputA.length - 1; i > 0; i--) {
+            if (inputB[currentB] > inputA[currentA]) {
+                inputA[i] = inputB[currentB];
+                currentB--;
             } else {
-                result[current] = input[low];
-                low++;
+                inputA[i] = inputA[currentA];
+                currentA--;
             }
-
-            current++;
-        }
-
-        while (low < midIndex) { // Fill in the result with the rest of low end
-            result[current] = input[low];
-            low++;
-            current++;
-        }
-
-        while(mid <= high) {  // Fil in the result with the rest of high end
-            result[current] = input[mid];
-            mid++;
-            current++;
-        }
-
-        for (int i = 0; i < input.length; i++) {  // Put results back into original array
-            input[i] = result[i];
         }
     }
 }
